86bac4f2f96c369141aeaa90556da1b1c2e38e7d
maradatech <maradatech@gmail.com>
2017-04-10 17:21:12 +0200

    Implemented extension method MaradaTech.Utils.SystemExtensions.IsLongerThan(uint).

---
+++ MaradaTech.Utils.SystemExtensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace MaradaTech.Utils.SystemExtensions
+{
+    /// <summary>
+    /// Extends <see cref="System.String"/>.
+    /// </summary>
+    public static class StringExtensions
+    {
+        /// <summary>
+        /// Determines whether a string is longer in length than the specified value.
+        /// </summary>
+        /// <param name="s">The string to process.</param>
+        /// <param name="value">The specified length.</param>
+        /// <returns><see cref="true"/> if the length of the string exceeds the value, false otherwise.</returns>
+        public static bool IslongerThan(this string s, uint value)
+        {
+            return s.Length > value;
+        }
+    }
+}
