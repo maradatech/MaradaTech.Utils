4d77eeb1ba0cb6f59d090f7f2a8226c34f182136
maradatech <maradatech@gmail.com>
2017-11-05 22:00:04 +0100

    Added more

---
+++ MaradaTech.Utils.System.IOExtensions/PathExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MaradaTech.Utils.SystemExtensions.IO
+{
+    public static class PathExtensions
+    {
+        public static string MakePathFrom(char pathSeperator = '/', params string[] values)
+        {
+            if(values.Any(s => s is null || s == String.Empty))
+                throw new ArgumentException("At least one of the provided parameters were null", nameof(values));
+            if(pathSeperator != '/' && pathSeperator != '\\')
+                throw new ArgumentException("Must be either '/' (default) or '\'", nameof(pathSeperator));
+            string path = String.Empty;
+            foreach(string item in values)
+            {
+                path += String.Concat(item, pathSeperator);
+            }
+            return path;
+        }
+    }
+}
--- MaradaTech.Utils.SystemExtensions/StringExtensions.cs
+++ MaradaTech.Utils.SystemExtensions/StringExtensions.cs
@@ -1,24 +1,41 @@
-﻿namespace MaradaTech.Utils.SystemExtensions
+﻿using System;
+using System.Linq;
+
+namespace MaradaTech.Utils.SystemExtensions
 {
     /// <summary>
     /// Extends <see cref="System.String"/>.
     /// </summary>
     public static class StringExtensions
     {
-        /// <summary>
-        /// Determines whether a string is longer in length than the specified value.
-        /// </summary>
+        /// <summary>Determines whether this string is longer in length than the specified <paramref name="value"/>.</summary>
         /// <param name="s">The string to process.</param>
         /// <param name="value">The specified length.</param>
         /// <returns>true if the length of the string exceeds the value, false otherwise.</returns>
-        public static bool IslongerThan(this string s, uint value) => s.Length > value;
+        public static bool IslongerThan(this string s, int value)
+            => (value >= 0) ? s.Length > value : throw new ArgumentOutOfRangeException(nameof(value), "must not be negative");
 
-        /// <summary>
-        /// Determines whether a string is shorter in length than the specified value.
-        /// </summary>
+        /// <summary>Determines whether this string is shorter in length than the specified <paramref name="value"/>.</summary>
         /// <param name="s">The string to process.</param>
         /// <param name="value">The specified length.</param>
         /// <returns>true if the length of the string does not exceed the value, false otherwise.</returns>
-        public static bool IsShorterThan(this string s, uint value) => s.Length < value;
+        public static bool IsShorterThan(this string s, int value)
+            => (value >= 0) ? s.Length < value : throw new ArgumentOutOfRangeException(nameof(value), "must not be negative");
+
+        /// <summary>Determines whether this string is equal to the length specified by the <paramref name="value"/>.</summary>
+        /// <param name="s">The string to process.</param>
+        /// <param name="value">The specified length.</param>
+        /// <returns>true if the length of the string exceeds the value, false otherwise.</returns>
+        public static bool LengthEquals(this string s, int value)
+            => (value >= 0) ? s.Length > value : throw new ArgumentOutOfRangeException(nameof(value), "must not be negative");
+
+        public static bool ContainsLettersOnly(this string s)
+            => s.All(c => Char.IsLetter(c));
+
+        public static bool ContainsDigitsOnly(this string s)
+            => s.All(c => Char.IsDigit(c));
+
+        public static bool ContainsLettersAndOrDigitsOnly(this string s)
+            => s.All(c => Char.IsLetterOrDigit(c));
     }
 }
