e0200d4c82ec6b1f4661ea9e016eafb517d58a81
maradatech <maradatech@gmail.com>
2017-11-05 20:54:10 +0100

    Added IsShorterThan method

--- MaradaTech.Utils.SystemExtensions/StringExtensions.cs
+++ MaradaTech.Utils.SystemExtensions/StringExtensions.cs
@@ -11,9 +11,14 @@
         /// <param name="s">The string to process.</param>
         /// <param name="value">The specified length.</param>
         /// <returns>true if the length of the string exceeds the value, false otherwise.</returns>
-        public static bool IslongerThan(this string s, uint value)
-        {
-            return s.Length > value;
-        }
+        public static bool IslongerThan(this string s, uint value) => s.Length > value;
+
+        /// <summary>
+        /// Determines whether a string is shorter in length than the specified value.
+        /// </summary>
+        /// <param name="s">The string to process.</param>
+        /// <param name="value">The specified length.</param>
+        /// <returns>true if the length of the string does not exceed the value, false otherwise.</returns>
+        public static bool IsShorterThan(this string s, uint value) => s.Length < value;
     }
 }
