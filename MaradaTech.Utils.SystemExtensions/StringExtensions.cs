2fe1f51ca19240f8680a2106eb1f5e05cb870651
maradatech <maradatech@gmail.com>
2018-04-08 16:03:04 +0200

    Added method: IsNumber (not tested).

--- MaradaTech.Utils.SystemExtensions/StringExtensions.cs
+++ MaradaTech.Utils.SystemExtensions/StringExtensions.cs
@@ -46,6 +46,30 @@ namespace MaradaTech.Utils.SystemExtensions
         /// <returns>A <see cref="System.Boolean"/> indicating whether the string contains only letters and/or digits.</returns>
 
         public static bool ContainsLettersAndOrDigitsOnly(this string s)
-            => s.All(c => Char.IsLetterOrDigit(c)); 
+            => s.All(c => Char.IsLetterOrDigit(c));
+
+        /// <summary>
+        /// Determines whether the contents of this instance is a number. Only one period or comma is accepted as a potential decimal separator.
+        /// </summary>
+        /// <param name="s">The string to test.</param>
+        /// <returns>
+        ///   <c>true</c> if the specified s is a number; otherwise, <c>false</c>.
+        /// </returns>
+        public static bool IsNumber(this string s)
+        {
+            s = s.Trim();
+            string[] c = s.Split(new char[] { ',', '.' });
+            if(c.Length == 1)
+                return System.Numerics.BigInteger.TryParse(c[0], out System.Numerics.BigInteger x);
+            else if(c.Length == 2)
+                if(System.Numerics.BigInteger.TryParse(c[0], out System.Numerics.BigInteger integralPart))
+                    if(System.Numerics.BigInteger.TryParse(c[1], out System.Numerics.BigInteger fractionalPart))
+                        if(fractionalPart > System.Numerics.BigInteger.Zero)
+                            return true;
+                        else return false;
+                    else return false;
+                else return false;
+            else return false;
+        }
     }
 }
